6a5ae2d3193f350b956cc1730df6e4d83af910a2
artem <temaartem240904@gmail.com>
2024-04-20 13:57:13 +0300

    ordering field

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-
+from .fields import OrderField
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -38,9 +38,13 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank = True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self) -> str:
-        return self.title
+        return f'{self.order}. {self.title}'
     
 class Content(models.Model):
     module = models.ForeignKey(Module,
@@ -56,6 +60,10 @@ class Content(models.Model):
                                      )})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank = True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 class ItemBase(models.Model):
     owner = models.ForeignKey(User,
