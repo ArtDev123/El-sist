3f90ca5c495e11a9e0711bd39fada0206c4c48ff
artem <temaartem240904@gmail.com>
2024-04-22 12:51:36 +0300

    perm class views

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
@@ -8,6 +8,7 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name = 'logout'),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls')),
 ]
 
 if settings.DEBUG:
